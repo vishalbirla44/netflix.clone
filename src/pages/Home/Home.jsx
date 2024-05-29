c7f1d68d6b0f91ff3df5f8e5b5551c64634b6c2c
vishal birla <vishalbirla26487@gmail.com>
2024-05-29 07:52:41 +0000

    3rd day

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../componands/Navbar/Navbar'
+import Hero_baner from '../../assets/hero_banner.jpg'
+import Hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className='home'>
+      <Navbar />
+      <div className='hero'>
+        <img src={Hero_baner} alt="" className='banner_img' />
+        <div className='hero-caption'>
+          <img src={Hero_title} alt="" className='caption-img' />
+          <p>Descovering  his ties to a secret ancient order, young man living in moderen stanbul embark on a quest to save the city from an importable enemy</p>
+        </div>
+      </div>
+
     </div>
   )
 }
