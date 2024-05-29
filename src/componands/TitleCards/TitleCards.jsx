17830296fbdac7d62f237bc708c3cd673d3aba51
vishal birla <vishalbirla26487@gmail.com>
2024-05-29 10:04:29 +0000

    3rd

--- src/componands/TitleCards/TitleCards.jsx
+++ src/componands/TitleCards/TitleCards.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/card/Cards_data'
 
 const TitleCards = () => {
   return (
-    <div className='TitleCards'>TitleCards</div>
+    <div className='TitleCards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card">
+        {cards_data.map((card,index)=>{
+return <div className="card" key={index}>
+<img src={card.image} alt="" />
+<p>{card.name}</p>
+</div>
+        })}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css'
 import Navbar from '../../componands/Navbar/Navbar'
 import Hero_baner from '../../assets/hero_banner.jpg'
 import Hero_title from '../../assets/hero_title.png'
+import Play_icon from '../../assets/play_icon.png'
+import Info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../componands/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -13,6 +16,18 @@ const Home = () => {
         <div className='hero-caption'>
           <img src={Hero_title} alt="" className='caption-img' />
           <p>Descovering  his ties to a secret ancient order, young man living in moderen stanbul embark on a quest to save the city from an importable enemy</p>
+          <div className="hero-btns">
+            <button className='btn'>
+              <img src={Play_icon} alt="" />
+              Play
+            </button>
+            
+            <button className='btn dark-btn'>
+              <img src={Info_icon} alt="" />
+               More Info
+            </button>
+          </div>
+          <TitleCards/> 
         </div>
       </div>
 
